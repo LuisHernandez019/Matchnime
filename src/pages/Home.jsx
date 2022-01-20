f54ec3f4a62f319c5a916efe85b5976c51894c4d
Luis Hernandez <uwu.luishernandez@gmail.com>
2022-01-20 17:28:30 -0600

    implement Home module

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import logo from '../assets/logo_matchnime.png';
+import homeGirl from '../assets/home_girl.svg';
+import playStore from '../assets/google-play.svg';
+import appStore from '../assets/app_store.svg';
+import '../styles/Home.scss'
 
-const NotFound = () => {
+const Home = () => {
+	const handleToggle = () => {
+		var menuToggle = document.querySelector('.toggle');
+      var navigation = document.querySelector('.navigation');
+
+      menuToggle.classList.toggle('active')
+      navigation.classList.toggle('active')
+	}
+
 	return (
-		<div>
-			Hola mundo (ahora si)
-		</div>
+		<section>
+			<div className="circle"></div>
+			<header>
+				<a href=""><img src={logo} className="logo" alt="logo" /></a>
+				<div className="toggle" onClick={handleToggle}></div>
+				<ul className="navigation">
+					<li><a href="#">About</a></li>
+					<li><a href="#">Features</a></li>
+					<li><a href="#">Download</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</header>
+			<div className="content">
+				<div className="textBox">
+					<h2>We found you a <span>Perfect Match!</span></h2>
+					<div className="buttons">
+						<a href="#" className="sign-up">Create account</a>
+						<a href="#" className="sign-in">Log in</a>
+					</div>
+				</div>
+				<img src={homeGirl} alt="homeGirl" className="homeGirl" />
+			</div>
+			<ul className="thumb">
+				<li><img src={playStore} alt="google play" /></li>
+				<li><img src={appStore} alt="app store" /></li>
+			</ul>
+		</section>
 	);
 }
 
-export default NotFound;
+export default Home;
